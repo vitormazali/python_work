8a2b69e959bc50163472bf086b00c42f88bcbad7
vmazali <vmazali@cisco.com>
2022-01-06 17:44:14 -0300

    string files

---
+++ famous_quote.py
@@ -0,0 +1,8 @@
+author_first_name = " albert  "
+author_last_name = "  einstein "
+author_quote = "A person who never made a mistake never tried anything new"
+printable_message = f"\t{author_quote.strip()}\n\n\t\t{author_last_name.title().strip()}, {author_first_name.title().strip()}"
+print(author_first_name)
+print(author_last_name)
+print(author_quote)
+print(f"\n\n\n{printable_message.strip()}")
---
+++ name.py
@@ -0,0 +1,6 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+print(f"\n\nHello {name.title()}, how are you today?")
