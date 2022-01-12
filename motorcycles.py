80ba64063a841abd88bef799a597bdf183c360d3
vmazali <vmazali@cisco.com>
2022-01-12 19:34:36 -0300

    working with lists

---
+++ motorcycles.py
@@ -0,0 +1,8 @@
+# this program stores and print messages regarding motorcycles manufactures
+
+motorcycles_brands = ['Honda','Suzuki','Yamaha','Triumph','Royal Enfield','Harley Davidson']
+
+print(f"\tThe {motorcycles_brands[0].title()} is likely to be the most famous in Brazil")
+
+the_desired_motorcycle = f"i wish I have money to buy a {motorcycles_brands[-1].title()} for riding on the weekends"
+print(f"\n\n{the_desired_motorcycle.title()}")
