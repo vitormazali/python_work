b693a6f79a3e49aa71628e0e462574b054992511
vmazali <vmazali@cisco.com>
2022-01-12 22:53:36 -0300

    motos

--- motorcycles.py
+++ motorcycles.py
@@ -6,3 +6,14 @@ print(f"\tThe {motorcycles_brands[0].title()} is likely to be the most famous in
 
 the_desired_motorcycle = f"i wish I have money to buy a {motorcycles_brands[-1].title()} for riding on the weekends"
 print(f"\n\n{the_desired_motorcycle.title()}")
+
+motorcycles = []
+motorcycles.append('low rider')
+motorcycles.append('shadow')
+motorcycles.append('iron883')
+motorcycles.insert(1,'fat boy')
+
+wished_motorcycles = motorcycles.pop()
+
+print(f"\n\n{motorcycles}")
+print(f"\n\n{wished_motorcycles}")
